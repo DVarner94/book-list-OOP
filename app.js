520aa9967c1093266809f2a7bf71daefc68334cd
Dominic Varner <dommy@MLBs-Lappy.local>
2020-10-07 09:16:00 -0700

    Book/UI Constructors + Event listeners

---
+++ app.js
@@ -0,0 +1,54 @@
+// Book Constructor
+function Book(title, author, rating) {
+    this.title = title;
+    this.author = author;
+    this.rating = rating;
+}
+
+// UI Constructor
+function UI() {}
+
+UI.prototype.addBookToList = function(book){
+    const list = document.getElementById('book-list');
+    // Creat tr element
+    const row = document.createElement('tr');
+    // Insert columns
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.rating}</td>
+    <td><a href="#" class = "delete">x</a></td>
+    `;
+
+    list.appendChild(row)
+}
+
+// Clear Fields
+UI.prototype.clearFields = function(){
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('rating').value = '';
+}
+
+// Event Listeners
+document.getElementById('book-form').addEventListener('submit',
+    function (e) {
+        const title = document.getElementById('title').value,
+            author = document.getElementById('author').value,
+            rating = document.getElementById('rating').value
+
+        // Instantiate book
+        const book = new Book(title, author, rating);
+
+        // Instantiate UI
+        const ui = new UI();
+
+        // Add book to list
+        ui.addBookToList(book)
+
+        // Clear Fields
+        ui.clearFields();
+
+        
+        e.preventDefault()
+    })
